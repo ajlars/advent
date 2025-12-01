64638d5271ab21e4e98197f1583367fdec5868ef
AJ Larson <aj.larson@awardco.com>
2025-12-01 11:22:43 -0700

    day1 done

--- advent_2025/advent.py
+++ advent_2025/advent.py
@@ -4,7 +4,7 @@ import solutions.day1
 try:
     day = int(sys.argv[1])
     part = int(sys.argv[2])
-    mode = sys.argv[3]
+    mode = sys.argv[3] if len(sys.argv) > 3 else ""
 except:
     print(f'Usage: "py advent.py <day_integer> <part_integer> <mode_string>"\nReceived args: {sys.argv[1:]}')
 
--- advent_2025/solutions/day1.py
+++ advent_2025/solutions/day1.py
@@ -2,7 +2,7 @@ day = __file__.split("\\")[-1][3:-3]
 f1 = open(f"inputs/day{day}_1.txt", "r")
 f2 = open(f"inputs/day{day}_2.txt", "r")
 input_1 = f1.read().splitlines()
-input_2 = f2.read().splitlines()
+input_2 = input_1
 test_1 = """L68
 L30
 R48
@@ -13,13 +13,61 @@ L1
 L99
 R14
 L82""".splitlines()
-test_2 = """""".splitlines()
+test_2 = test_1
+
+class dial():
+    def __init__(self, start=50, size=100):
+        self.start = start
+        self.size = size
+        self.position = self.start
+        self.zero_stops = 0
+        self.zero_clicks = 0
+
+    def step(self, instruction):
+        direction = instruction[0]
+        _steps = int(instruction[1:])
+        self.zero_clicks += _steps // self.size
+        steps = _steps % self.size
+        if direction == 'L':
+            if(self.position-steps)<=0 and self.position!=0:
+                self.zero_clicks += 1
+            self.turn_left(steps)
+        elif direction == 'R':
+            if(self.position+steps)>=self.size and self.position!=0:
+                self.zero_clicks += 1
+            self.turn_right(steps)
+        if self.position == 0:
+            self.zero_stops += 1
+
+    def turn_left(self, steps):
+        self.position -= steps
+        if self.position < 0:
+            self.position += self.size
+
+    def turn_right(self, steps):
+        self.position += steps
+        if self.position >= self.size:
+            self.position -= self.size
+
+    def get_zero_stops(self):
+        return self.zero_stops
+
+    def get_zero_clicks(self):
+        return self.zero_clicks
+    
 
 def solution_1(input):
-    print("Solution 1 not yet implemented")
+    _dial = dial()
+    for line in input:
+        _dial.step(line)
+    print(_dial.get_zero_stops())
+
 
 def solution_2(input):
-    print("Solution 2 not yet implemented")
+    _dial = dial()
+    for line in input:
+        _dial.step(line)
+    print(_dial.get_zero_clicks())
 
 def main(part, mode):
     input = None
