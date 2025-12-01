369e502713038de7b5efa6e52faa27313cadf8ab
AJ Larson <aj.larson@awardco.com>
2025-12-01 10:24:14 -0700

    setup scripting and boilerplate

---
+++ advent_2025/advent.py
@@ -0,0 +1,23 @@
+import sys
+import solutions.day1
+
+try:
+    day = int(sys.argv[1])
+    part = int(sys.argv[2])
+    mode = sys.argv[3]
+except:
+    print(f'Usage: "py advent.py <day_integer> <part_integer> <mode_string>"\nReceived args: {sys.argv[1:]}')
+
+def main():
+    s = solutions
+    solution = None
+    if not day:
+        return
+    match day:
+        case 1:
+            s.day1.main(part, mode)
+        case _:
+            print(f'No matching day configured for day {day}')
+
+if __name__ == "__main__":
+    main()
---
+++ advent_2025/solutions/day0.py
@@ -0,0 +1,23 @@
+day = __file__.split("\\")[-1][3:-3]
+f1 = open(f"inputs/day{day}_1.txt", "r")
+f2 = open(f"inputs/day{day}_2.txt", "r")
+input_1 = f1.read().splitlines()
+input_2 = f2.read().splitlines()
+test_1 = """""".splitlines()
+test_2 = """""".splitlines()
+
+def solution_1(input):
+    print("Solution 1 not yet implemented")
+
+def solution_2(input):
+    print("Solution 2 not yet implemented")
+
+def main(part, mode):
+    input = None
+    solution = solution_1 if part == 1 else solution_2
+    if mode == "test":
+        input = test_1 if part == 1 else test_2
+    else:
+        input = input_1 if part == 1 else input_2
+    print(f'Running Advent of Code 2025: Day {day}, Part {part}, Mode {mode}')
+    solution(input)
